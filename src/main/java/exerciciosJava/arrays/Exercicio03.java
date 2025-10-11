69321c727a7eece00298ce8264edb5c28cec04ef
Fernanda Dias <mariadosantodias@gmail.com>
2025-10-10 23:48:24 -0300

    exercicios 03

---
+++ src/main/java/exerciciosJava/arrays/Exercicio03.java
@@ -0,0 +1,14 @@
+package exerciciosJava.arrays;
+
+public class Exercicio03 {
+    public static void main(String[] args){
+        /*
+        Solicite ao usuário que digite 5 números (valores reais) e,
+        após receber todos os números, calcule e exiba a média aritmética
+        esses valores
+
+        a média é obtida somendo todos os números e dividindo pelo total
+         */
+        
+    }
+}
