aa1015542f564c2c1b2bcc077927194fbd59e0ac
Fernanda Dias <mariadosantodias@gmail.com>
2025-10-14 00:12:19 -0300

    exemplo 02

---
+++ src/main/java/exerciciosJava/arrays/matrizes/Exemplo02.java
@@ -0,0 +1,13 @@
+package exerciciosJava.arrays.matrizes;
+
+public class Exemplo02 {
+    public static void main(String[] args){
+        /*
+        crie uma matriz de inteiros com 3 linhas e 3 colunas (3x3)
+        preenchido com valores fixos
+        Em seguida, percorra essa matriz e exiba seus elementos
+        no formato de tabela (linha por linha)
+         */
+
+    }
+}
