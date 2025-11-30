e9e3c6e6cbd7c77bc0c1e104c86412a9afd2c6af
Fernanda Dias <mariadosantodias@gmail.com>
2025-11-30 06:41:14 -0300

    exercicio 07

---
+++ src/main/java/CursoJava/aComplemento/Parte5/list/Exercicio07.java
@@ -0,0 +1,9 @@
+package CursoJava.aComplemento.Parte5.list;
+
+public class Exercicio07 {
+    /*
+   Iterador
+   Percorra uma lista com Iterator e remova todos os elementos que comecem com letra 'A'.
+   Hint: usar Iterator.remove() ao iterar.
+     */
+}
