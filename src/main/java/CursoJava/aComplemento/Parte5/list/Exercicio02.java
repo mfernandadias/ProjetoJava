48661721a52f1f479e3271bd41528d0cef8c03f6
Fernanda Dias <mariadosantodias@gmail.com>
2025-11-30 06:40:46 -0300

    exercicio 02

---
+++ src/main/java/CursoJava/aComplemento/Parte5/list/Exercicio02.java
@@ -0,0 +1,9 @@
+package CursoJava.aComplemento.Parte5.list;
+
+public class Exercicio02 {
+    public static void main(String[] args){
+        /*
+        criar uma LiskedList de números e imprimir
+         */
+    }
+}
