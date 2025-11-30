c38ee2bc8cbd71d622011138229f792e907152c6
Fernanda Dias <mariadosantodias@gmail.com>
2025-11-30 06:46:34 -0300

    exercicio 05

---
+++ src/main/java/CursoJava/aComplemento/parte7Strings/Exercicio05.java
@@ -0,0 +1,19 @@
+package CursoJava.aComplemento.parte7Strings;
+
+public class Exercicio05 {
+
+    public static void retornar(String palavra){
+        System.out.println(palavra.toUpperCase());
+        System.out.println(palavra.toLowerCase());
+    }
+
+    //exercicio 06 - tirar os espaços ao redor da string
+
+    public static void main(String[] args){
+        /*
+        receba uma string e retorne a mesma maiúsuclas e minúsculas
+         */
+
+        retornar("Fernanda");
+    }
+}
