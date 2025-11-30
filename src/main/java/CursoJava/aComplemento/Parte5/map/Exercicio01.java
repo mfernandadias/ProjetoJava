71e5c460103159fb97ef1208fa67daff270e8132
Fernanda Dias <mariadosantodias@gmail.com>
2025-11-30 06:43:02 -0300

    exercício 01

---
+++ src/main/java/CursoJava/aComplemento/Parte5/map/Exercicio01.java
@@ -0,0 +1,16 @@
+package CursoJava.aComplemento.Parte5.map;
+
+import java.util.HashMap;
+
+public class Exercicio01 {
+    public static void main(String[] args){
+        /*
+        crie um HashMap<String, Integer> para armazenar nomes e idades.
+
+         */
+        HashMap<String, Integer> cadastro= new HashMap<>();
+
+        cadastro.put("Fernanda", 17);
+
+    }
+}
