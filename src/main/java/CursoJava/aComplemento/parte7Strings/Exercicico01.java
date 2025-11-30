ab8013219213882ba0f734e57f7698395b644ccb
Fernanda Dias <mariadosantodias@gmail.com>
2025-11-30 06:45:34 -0300

    exercício 01

---
+++ src/main/java/CursoJava/aComplemento/parte7Strings/Exercicico01.java
@@ -0,0 +1,13 @@
+package CursoJava.aComplemento.parte7Strings;
+
+public class Exercicico01 {
+    public static void main(String[] args){
+        /*
+
+         */
+        String nome = "Fernanda";
+
+        System.out.println(nome.length());
+
+    }
+}
