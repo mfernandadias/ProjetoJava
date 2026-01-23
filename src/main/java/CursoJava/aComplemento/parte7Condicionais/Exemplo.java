fc5efb66513cc16027c539b0a055d1f474359ef5
Fernanda Dias <mariadosantodias@gmail.com>
2026-01-22 22:07:59 -0300

    exemplo java

---
+++ src/main/java/CursoJava/aComplemento/parte7Condicionais/Exemplo.java
@@ -0,0 +1,14 @@
+package CursoJava.aComplemento.parte7Condicionais;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        //Operador Ternário
+        int idade = 20;
+
+        String resultado = (idade >= 18) ? "Maior Idade" : "Menor de idade";
+
+        System.out.println(resultado);
+
+    }
+
+}
