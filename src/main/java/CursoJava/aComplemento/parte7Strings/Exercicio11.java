9c91c5ec8c399479ed068f8b4a8d2386cef63cf1
Fernanda Dias <mariadosantodias@gmail.com>
2026-01-22 22:08:41 -0300

    exercicio 07

---
+++ src/main/java/CursoJava/aComplemento/parte7Strings/Exercicio11.java
@@ -0,0 +1,18 @@
+package CursoJava.aComplemento.parte7Strings;
+
+/*
+11. Receba uma frase e transforme-a em uma frase onde cada palavra começa com letra maiúscula.
+12. Remova todas as vogais de uma string.
+13. Gere o acrônimo de uma frase usando a primeira letra de cada palavra.
+14. Valide se uma string está no formato de telefone `(xx) xxxx-xxxx` ou `(xx) xxxxx-xxxx`.
+15. Remova caracteres consecutivos repetidos em uma string.
+ */
+
+public class Exercicio11 {
+
+    public static void main(String[] args){
+
+    }
+
+
+}
