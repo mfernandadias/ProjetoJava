381afddea3392b8f167649c26fe90237487246a3
Fernanda Dias <mariadosantodias@gmail.com>
2026-01-22 22:09:10 -0300

    exercicio 01

---
+++ src/main/java/CursoJava/aExercicios200/capitulo03Operadores/Exercicio01.java
@@ -0,0 +1,13 @@
+package CursoJava.aExercicios200.capitulo03Operadores;
+
+public class Exercicio01 {
+    public static void main(String[] args){
+        int i = 9;
+        int b = 10;
+        int g = 9;
+
+        double media = (i + b + g) / 3;
+
+        System.out.println("Media: " + media);
+    }
+}
