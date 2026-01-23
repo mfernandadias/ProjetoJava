ee352b4836b74bcc0f2da749e9725dd094108334
Fernanda Dias <mariadosantodias@gmail.com>
2026-01-22 22:08:51 -0300

    exemplo 01

---
+++ src/main/java/CursoJava/aComplemento/parte8Funcao/Exemplo01.java
@@ -0,0 +1,12 @@
+package CursoJava.aComplemento.parte8Funcao;
+
+public class Exemplo01 {
+    /*
+    o que é uma função?
+    De forma geral, função é um bloco de código que:
+    Executa uma tarefa especifica
+    Pode receber parâmetros
+    Pode ou não retorna um valor
+
+     */
+}
