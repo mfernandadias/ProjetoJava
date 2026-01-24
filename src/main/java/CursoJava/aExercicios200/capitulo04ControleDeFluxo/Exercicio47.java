82cdfa6d603259749df9113f253b6c55634a81e8
Fernanda Dias <mariadosantodias@gmail.com>
2026-01-23 21:29:41 -0300

    exercicio 03

---
+++ src/main/java/CursoJava/aExercicios200/capitulo04ControleDeFluxo/Exercicio47.java
@@ -0,0 +1,14 @@
+package CursoJava.aExercicios200.capitulo04ControleDeFluxo;
+
+public class Exercicio47 {
+    /*
+    contagem decrescente
+    Enunciado: Crie um programa que exiba os números de 10 a 1, em ordem decrescente usando
+    um laço for
+     */
+    public static void main(String[] args){
+        for(int i = 10; i >= 1; i--){
+            System.out.println(i);
+        }
+    }
+}
