2391b88db85d4b8f090d8e6ec954b03b32f84d49
Fernanda Dias <mariadosantodias@gmail.com>
2026-02-02 22:41:31 -0300

    exercicio 63

---
+++ src/main/java/CursoJava/aExercicios200/capitulo04ControleDeFluxo/Exercicio63.java
@@ -0,0 +1,14 @@
+package CursoJava.aExercicios200.capitulo04ControleDeFluxo;
+
+import java.util.Scanner;
+
+public class Exercicio63 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Crie uma senha");
+        String senha = scan.nextLine();
+
+
+    }
+}
