0079402cff8b0f0e86efa9b5bc93555ac1cb9e59
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-18 22:09:54 -0300

    Create Desafio07b.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo10Desafios/Desafio07b.java
@@ -0,0 +1,14 @@
+package CursoJava.aExercicios200.capitulo10Desafios;
+
+public class Desafio07b {
+    public static void main(String[] args){
+        /*
+        escreva um programa que receba uma string e conte quantas
+        vogair existem nela
+         */
+        String nome = "fernanda";
+        for(int i = 0; i < nome.length(); i++){
+
+        }
+    }
+}
