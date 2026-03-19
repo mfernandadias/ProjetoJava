e9d4c9ff3570019bc7a462f7d8a66221c750e8ce
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-18 22:10:54 -0300

    Create Exercicio10.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo2/Exercicio10.java
@@ -0,0 +1,15 @@
+package CursoJava.aExercicios200.capitulo2;
+
+public class Exercicio10 {
+    public static void main(String[] args){
+        /*
+        desenvolva um programa que exiba o valor de uma variável double com duas
+        casas decimais. Utilize formatação para garantir que o valor seja exibido
+        corretamente
+         */
+        double altura = 1.8790;
+
+        System.out.printf("%.2f%n", altura);
+
+    }
+}
