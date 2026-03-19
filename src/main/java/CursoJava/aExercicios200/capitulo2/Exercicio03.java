17f92bd9e9c0b6eca2a2d1f3493e06144b17d0cd
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-18 22:10:36 -0300

    Create Exercicio03.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo2/Exercicio03.java
@@ -0,0 +1,21 @@
+package CursoJava.aExercicios200.capitulo2;
+
+public class Exercicio03 {
+    /*
+    crie um programa que declare e inicialize variáveis de todos os tipos
+    primitivos em Java(int, double, char, boolean, etc). Exiba o valor de
+    cada variável no console
+     */
+    public static void main(String[] args){
+        int idade = 25;
+        double altura = 1.75;
+        char inicial = 'M';
+        boolean estudante = true;
+
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(inicial);
+        System.out.println(estudante);
+
+    }
+}
