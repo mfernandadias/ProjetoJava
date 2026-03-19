126eb8435337c8d82d655b3580ff8ad1830297d0
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-18 22:10:45 -0300

    Create Exercicio06.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo2/Exercicio06.java
@@ -0,0 +1,15 @@
+package CursoJava.aExercicios200.capitulo2;
+
+public class Exercicio06 {
+    public static void main(String[] args){
+        /*
+        crie um programa que utilize a palavra-chave final para declarar
+        uma constante que representa a velocidade da luz no vácuo. Tente
+        alterar o valor da constante e observe o comportamento do compilador
+         */
+        final double velocidadeDaluz = 8.000;
+
+        
+
+    }
+}
