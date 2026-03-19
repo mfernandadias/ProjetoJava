ac702b79758cf19f8afc5d594d22a2cb7b7803f0
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-18 22:10:39 -0300

    Create Exercicio04.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo2/Exercicio04.java
@@ -0,0 +1,17 @@
+package CursoJava.aExercicios200.capitulo2;
+
+public class Exercicio04 {
+    public static void main(String[] args){
+
+        double altura = 1.75;
+
+        int valorInt = (int) altura;
+        int numero = 10;
+        double numeroConvertido = numero;
+
+
+
+
+        System.out.println();
+    }
+}
