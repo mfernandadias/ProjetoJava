bd98bb2d066fd3e388c20d586ef39508b0084a67
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-18 22:10:26 -0300

    Create Desafio10.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo10Desafios/Desafio10.java
@@ -0,0 +1,16 @@
+package CursoJava.aExercicios200.capitulo10Desafios;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class Desafio10 {
+    /*
+    crie um programa que gere senhas aleatória de 8 a 16 caracteres, incluindo
+    letras, números e símbolos
+     */
+
+    //final - não pode ser mudado 
+    private static final String CARACTERES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=<>?";
+
+
+}
