44fef6d4e39fde74f736b9066a21d86b37c82015
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-24 20:28:32 -0300

    Create Exercicio110.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo6MetodosEFuncoes105/Exercicio110.java
@@ -0,0 +1,17 @@
+package CursoJava.aExercicios200.capitulo6MetodosEFuncoes105;
+
+public class Exercicio110 {
+    /*
+    crie um método chamado obterSaudacao() que retorna 9a string
+    "Olá, seja bem-vindo"
+     */
+    public static String mensagem(){
+        return "OLá, seja bem vindo";
+    }
+    public static void main(String[] args){
+        String mensagem = mensagem();
+
+        System.out.println(mensagem);
+
+    }
+}
