dae821bb3deed64f3fb6fdc484abef5eb81af3cc
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-24 20:20:28 -0300

    Create Exercicio82.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo5Arrays/Exercicio82.java
@@ -0,0 +1,24 @@
+package CursoJava.aExercicios200.capitulo5Arrays;
+
+public class Exercicio82 {
+    /*
+    escreva um programa que leia uma matriz 3x3 e exiba a soma dos elementos de
+    cada linha e de cada coluna
+     */
+    public static void main(String[] args){
+        int[][] valores = {
+                {2, 5, 6},
+                {4, 6, 9},
+                {5, 7, 8}
+        };
+        int[] somaLinhas = new int[3];
+        int[] somaColunas = new int[3];
+
+        //preenchendo a matriz
+
+
+
+
+
+    }
+}
