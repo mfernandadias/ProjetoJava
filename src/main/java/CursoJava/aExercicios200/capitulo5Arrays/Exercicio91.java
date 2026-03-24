194ae3dbd7cb520337eeb1ee3b894e38fe58e388
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-24 20:25:46 -0300

    Create Exercicio91.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo5Arrays/Exercicio91.java
@@ -0,0 +1,20 @@
+package CursoJava.aExercicios200.capitulo5Arrays;
+import java.util.Scanner;
+
+public class Exercicio91 {
+    /*
+    desenvolva um programa que leia 10 números inteiros, or ordene em ordem crescente e,
+    em seguida, utilize o método de busca binária para encontrar um número fornecido pelo
+    usuário. Exiba a posição do número se ele dor encontrado
+     */
+    public static void main(String[] args){
+      int[] numeros = {9, 10, 2, 3, 4, 5, 6, 7, 0};
+
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Digite um valor");
+      int num = scan.nextInt();
+
+
+
+    }
+}
