5fe0e55ea8fba0dea0b67deb8b49092dd4ce7b0f
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-24 20:19:44 -0300

    Create Exercicio74.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo5Arrays/Exercicio74.java
@@ -0,0 +1,16 @@
+package CursoJava.aExercicios200.capitulo5Arrays;
+
+public class Exercicio74 {
+    public static void main(String[] args){
+        /*
+
+
+         */
+        int[] valor = {7, 5, 3, 21, 43, 2, 4};
+
+        for(int i = 1; i < valor.length; i+= 2){
+                System.out.print(valor[i] + " ");
+
+        }
+    }
+}
