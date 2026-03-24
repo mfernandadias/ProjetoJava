d07fa910d1ab07980a8cc391761b8a7db6842d1c
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-24 20:27:42 -0300

    Create Exercicio106.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo6MetodosEFuncoes105/Exercicio106.java
@@ -0,0 +1,17 @@
+package CursoJava.aExercicios200.capitulo6MetodosEFuncoes105;
+
+public class Exercicio106 {
+    /*
+    implemente um método chamado exibirNumero() que receba um
+    número inteiro como parâmetro ee xiba esse numero no console.
+    no método main(), chame o método passando diferente valores para testar
+     */
+    public static Integer num(int valor){
+        return valor;
+    }
+    public static void main(String[] args){
+
+        System.out.println(num(10));
+
+    }
+}
