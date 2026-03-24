49966b17350bd7c155f0dfc9402f881a03e44f1c
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-24 20:28:20 -0300

    Create Exercicio107.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo6MetodosEFuncoes105/Exercicio107.java
@@ -0,0 +1,15 @@
+package CursoJava.aExercicios200.capitulo6MetodosEFuncoes105;
+
+public class Exercicio107 {
+    /*
+    soma de dois números
+    crie um metodo chamado somar() que receba dois números inteiros como parâmetro calcule a soma deles
+    e exiba o resultado no console.
+     */
+    public static void soma(int a, int b){
+        System.out.println("Soma: " + (a + b));
+    }
+    public static void main(String[] args){
+        soma(3, 5);
+    }
+}
