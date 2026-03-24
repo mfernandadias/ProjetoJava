bedb11e04d8e1e770d791329d4b3864e409c369b
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-24 20:27:25 -0300

    Create ExemploArrayList.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo5RevisaoArrays/ExemploArrayList.java
@@ -0,0 +1,11 @@
+package CursoJava.aExercicios200.capitulo5RevisaoArrays;
+
+public class ExemploArrayList {
+    /*
+    cresce dinamicamente
+    acesso rápido por índice
+    Mais lendo para remoção no meio
+
+    ArrayList<String> nomes = new ArrayList<>();
+     */
+}
