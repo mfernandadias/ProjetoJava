9c19e4f58482671dae8d7c6b1bf8c127f1c74ef7
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-24 20:27:33 -0300

    Create Exercicio105.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo6MetodosEFuncoes105/Exercicio105.java
@@ -0,0 +1,15 @@
+package CursoJava.aExercicios200.capitulo6MetodosEFuncoes105;
+
+public class Exercicio105 {
+    /*
+    crie um método chamado saudacao() que exiba a mensagem
+    "Olá
+     */
+    public static void mensagem(){
+        System.out.println("Ola");
+    }
+
+    public static void main(String[] args){
+        mensagem();
+    }
+}
