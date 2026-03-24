f2d59ecd46d7418a39218e16af1be6292224142d
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-24 20:27:22 -0300

    Create Exercicio71.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo5Arrays/revisao/Exercicio71.java
@@ -0,0 +1,11 @@
+package CursoJava.aExercicios200.capitulo5Arrays.revisao;
+
+public class Exercicio71 {
+    /*
+    Crie um progroma que leia 6 números inteiros e armazene-os em um array
+    Depois, exiba os valores do array na ordem inversa
+     */
+    public static void main(String[] args){
+
+    }
+}
