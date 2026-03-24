5db3d2f13355f1921cfacff2b47257cfd3d8a1f1
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-24 20:25:49 -0300

    Create Exercicio92.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo5Arrays/Exercicio92.java
@@ -0,0 +1,18 @@
+package CursoJava.aExercicios200.capitulo5Arrays;
+
+public class Exercicio92 {
+    /*
+    implemente o algortimo de ordenação Bubble Sort para ordenar
+    um array de 10 números inteiros em ordem crescente
+     */
+    public static void main(String[] args){
+        int[] num = {9, 10,3, 4, 3, 2, 1, 3, 4};
+
+        //algortimo de ordenação Bublle Sort
+        for(int i = 0; i < num.length - 1; i++){
+            for(int j = 0; j < num.length - 1 - i; j++){
+
+            }
+        }
+    }
+}
