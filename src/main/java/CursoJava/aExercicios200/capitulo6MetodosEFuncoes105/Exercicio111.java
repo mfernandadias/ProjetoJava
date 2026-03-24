9912b9b3c88d6f74d080fee918db44baf3440e03
Fernanda Dias <mariadosantodias@gmail.com>
2026-03-24 20:28:35 -0300

    Create Exercicio111.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo6MetodosEFuncoes105/Exercicio111.java
@@ -0,0 +1,16 @@
+package CursoJava.aExercicios200.capitulo6MetodosEFuncoes105;
+
+public class Exercicio111 {
+    /*
+    soma com retorno
+    */
+    public static Integer soma(int a, int b){
+        return a + b;
+    }
+
+    public static void main(String[] args){
+        int somar = soma(9, 8);
+
+        System.out.println(somar); 
+    }
+}
