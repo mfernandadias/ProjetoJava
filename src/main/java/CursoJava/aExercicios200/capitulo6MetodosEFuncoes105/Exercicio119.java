0ddcbef8d41c9cafdd7299ba2460874344eb6629
Fernanda Dias <mariadosantodias@gmail.com>
2026-04-02 00:18:21 -0300

    Create Exercicio119.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo6MetodosEFuncoes105/Exercicio119.java
@@ -0,0 +1,12 @@
+package CursoJava.aExercicios200.capitulo6MetodosEFuncoes105;
+
+public class Exercicio119 {
+    /*
+    implemente um método chamado calcularRaizQuadrado() que receba um número
+    inteiro e
+     */
+
+    public static void main(String[] args){
+
+    }
+}
