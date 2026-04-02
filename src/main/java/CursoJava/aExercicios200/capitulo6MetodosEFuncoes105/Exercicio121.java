821d709c1cd927c1c64a35eefbfdbf88895d8767
Fernanda Dias <mariadosantodias@gmail.com>
2026-04-02 00:18:32 -0300

    Create Exercicio121.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo6MetodosEFuncoes105/Exercicio121.java
@@ -0,0 +1,17 @@
+package CursoJava.aExercicios200.capitulo6MetodosEFuncoes105;
+
+public class Exercicio121 {
+    /*
+    verificar número primo
+    desenvolca um método chaamdo hePrimo() que receba um numero inteiro e retorne true
+    se o número for primo, e false caso contrário. No método main(), utilize o valor retornado
+    para exibir a mensagem correspondente
+     */
+    /*public static boolean valor(int valor){
+        if(valor < 2 ){
+            return false;
+        }
+
+
+    } */
+}
