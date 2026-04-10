2bccf95919f6edfd0bb38ba662627da7ba95dffc
Fernanda Dias <mariadosantodias@gmail.com>
2026-04-09 23:38:55 -0300

    Create Exercicio01.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo6MetodosRevisao/Exercicio01.java
@@ -0,0 +1,12 @@
+package CursoJava.aExercicios200.capitulo6MetodosRevisao;
+
+public class Exercicio01 {
+    public static void mensagem(){
+        System.out.println("Bem-Vindo ao Java");
+    }
+
+    public static void main(String[] args){
+        mensagem();
+
+    }
+}
