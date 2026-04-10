b3437ad76527e4b8e3bc04bb6db35f1f81b72f54
Fernanda Dias <mariadosantodias@gmail.com>
2026-04-09 23:38:47 -0300

    Create Exercicio127.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo6MetodosEFuncoes105/Exercicio127.java
@@ -0,0 +1,18 @@
+package CursoJava.aExercicios200.capitulo6MetodosEFuncoes105;
+
+public class Exercicio127 {
+
+    public static void mensagem(){
+        System.out.println("Olá");
+    }
+
+    public static void mensagemNome(String nome){
+        System.out.println("OLá  " + nome);
+    }
+    public static void main(String[] args){
+        mensagem();
+
+        mensagemNome("Fernanda");
+
+    }
+}
