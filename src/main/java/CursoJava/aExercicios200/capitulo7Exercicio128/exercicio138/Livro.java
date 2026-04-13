62317d4d33c1d4525ca53d4ec298b397396e0221
Fernanda Dias <mariadosantodias@gmail.com>
2026-04-12 21:01:43 -0300

    Create Livro.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo7Exercicio128/exercicio138/Livro.java
@@ -0,0 +1,23 @@
+package CursoJava.aExercicios200.capitulo7Exercicio128.exercicio138;
+
+public class Livro {
+    String titulo;
+    String autor;
+    int numeroPaginas;
+
+    public void exibirDetalhes(){
+        System.out.println(
+                "Titulo: " + titulo +
+                        "Autor: " + autor +
+                        "Paginas" + numeroPaginas
+        );
+    }
+
+    public static void main(String[] args){
+        Livro livro1 = new Livro();
+
+
+
+
+    }
+}
