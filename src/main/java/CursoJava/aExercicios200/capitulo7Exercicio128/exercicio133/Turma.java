3de467499f866140a0d81581a350fcfc73fddd51
Fernanda Dias <mariadosantodias@gmail.com>
2026-04-12 21:01:22 -0300

    Create Turma.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo7Exercicio128/exercicio133/Turma.java
@@ -0,0 +1,10 @@
+package CursoJava.aExercicios200.capitulo7Exercicio128.exercicio133;
+
+public class Turma {
+    String nome;
+
+    public void ExibirNome(){
+        System.out.println("Nome turma: " + nome);
+    }
+
+}
