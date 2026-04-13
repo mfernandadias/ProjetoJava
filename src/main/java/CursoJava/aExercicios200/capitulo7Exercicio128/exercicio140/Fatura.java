6b82f9c2c485ec15f15f5236734428dd3dd50ba2
Fernanda Dias <mariadosantodias@gmail.com>
2026-04-12 21:01:53 -0300

    Create Fatura.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo7Exercicio128/exercicio140/Fatura.java
@@ -0,0 +1,8 @@
+package CursoJava.aExercicios200.capitulo7Exercicio128.exercicio140;
+
+public class Fatura {
+    /*
+    implementa uma classe Fatura com atributo numeroItem, descricao,
+    quantidade, precoPorItem
+     */
+}
