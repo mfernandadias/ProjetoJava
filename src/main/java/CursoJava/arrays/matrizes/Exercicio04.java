bcc841d2c09b4a08436571cf67ed0e11a3254b23
Fernanda Dias <mariadosantodias@gmail.com>
2026-05-11 07:38:59 -0300

    Create Exercicio04.java

---
+++ src/main/java/CursoJava/arrays/matrizes/Exercicio04.java
@@ -0,0 +1,17 @@
+package CursoJava.arrays.matrizes;
+
+public class Exercicio04 {
+    public static void main(String[] args){
+        /*
+        dada uma matriz 3x3 com valores unteiros fixos,
+        troque a primeira linha com a última
+         */
+        int[][] matrizA = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+
+    }
+}
