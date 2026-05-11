19638f4dd514493864a9c4f8ad725664701825f2
Fernanda Dias <mariadosantodias@gmail.com>
2026-05-11 07:34:56 -0300

    Create Turma.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo7Exercicio128/Turma.java
@@ -0,0 +1,18 @@
+package CursoJava.aExercicios200.capitulo7Exercicio128;
+
+public class Turma {
+    /*
+   Crie duas classes: Aluno e Turma. A classe Turma deve ter o atributo nome e
+um método para exibir o nome da turma. A classe Aluno deve ter os atributos nome e turma,
+sendo turma do tipo Turma. No método main(), crie objetos das duas classes e exiba as
+informações.
+
+     */
+    String nome;
+
+    public void nomeTurma(String nome){
+
+    }
+
+
+}
