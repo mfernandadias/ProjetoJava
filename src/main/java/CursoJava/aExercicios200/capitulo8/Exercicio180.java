9fe58414d5a1ce274a16c6d1528978738b54b7eb
Fernanda Dias <mariadosantodias@gmail.com>
2026-05-11 07:35:35 -0300

    Create Exercicio180.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo8/Exercicio180.java
@@ -0,0 +1,24 @@
+package CursoJava.aExercicios200.capitulo8;
+
+public class Exercicio180 {
+    public static void main(String[] args){
+        /*
+
+         */
+        String nome = "fernanda";
+
+        System.out.println(nome.length());
+
+        //comparar duas strings
+        String palavra1 = "olho";
+        String palavra2 = "olho";
+
+
+
+        if(palavra1.equals(palavra2)){
+            System.out.println("palavra iguais");
+        }else{
+            System.out.println("Palavras diferentes");
+        }
+    }
+}
