1d058e759bec746c229faf470483b5a7e20a9d20
Fernanda Dias <mariadosantodias@gmail.com>
2026-05-11 07:37:35 -0300

    Create Exercicio188.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo9Strings/Exercicio188.java
@@ -0,0 +1,14 @@
+package CursoJava.aExercicios200.capitulo9Strings;
+
+public class Exercicio188 {
+    public static void main(String[] args){
+        /*
+        implemente um programa que receba uma string  substitua todas as
+        ocorrências de um caractere por outro,
+        fornecdios pelo usuário
+         */
+        String text = "fernanda maria jair alfredo bella ";
+
+        System.out.println(text.replace("a", "#"));
+    }
+}
