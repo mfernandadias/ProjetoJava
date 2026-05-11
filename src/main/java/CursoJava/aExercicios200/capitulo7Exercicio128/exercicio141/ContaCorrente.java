f6689bce3ddca00b4ac99abbb04a3f43c140dbbc
Fernanda Dias <mariadosantodias@gmail.com>
2026-05-11 07:34:30 -0300

    Create ContaCorrente.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo7Exercicio128/exercicio141/ContaCorrente.java
@@ -0,0 +1,10 @@
+package CursoJava.aExercicios200.capitulo7Exercicio128.exercicio141;
+
+public class ContaCorrente {
+    String numeroConta;
+    double saldo;
+    double limite;
+
+    //método para verificar se o saldo está dentro do limite
+
+}
