f6b33bd4fec3ae1b3d4ebe7aa12c54f6f7433fc3
Fernanda Dias <mariadosantodias@gmail.com>
2026-05-11 07:35:26 -0300

    Create ExemploArrays.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo8/ExemploArrays.java
@@ -0,0 +1,12 @@
+package CursoJava.aExercicios200.capitulo8;
+
+public class ExemploArrays {
+    public static void main(String[] args){
+        StringBuilder nome = new StringBuilder("Fernanda");
+
+
+        StringBuilder nomeInverso = nome.reverse();
+
+        System.out.println(nomeInverso);
+    }
+}
