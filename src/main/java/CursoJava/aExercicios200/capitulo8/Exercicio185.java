7c8b1eef2030104dcfadf705db78002ba31d1568
Fernanda Dias <mariadosantodias@gmail.com>
2026-05-11 07:35:49 -0300

    Create Exercicio185.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo8/Exercicio185.java
@@ -0,0 +1,14 @@
+package CursoJava.aExercicios200.capitulo8;
+
+public class Exercicio185 {
+    public static void main(String[] args){
+        /*
+        implemente um programa que receba uma string do usuário e remova todos
+        os espaços em branco, exibindo o resultado
+         */
+        String nome = "Maria Fernanda dos Santos Dias";
+
+        String textoSemEspaco = nome.replace(" ", "");
+
+    }
+}
