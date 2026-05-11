c5d22d4759d52b1206df767171cfab50d0d18a77
Fernanda Dias <mariadosantodias@gmail.com>
2026-05-11 07:38:48 -0300

    Create Exemplo01.java

---
+++ src/main/java/CursoJava/anteSpringFase01/Exemplo01.java
@@ -0,0 +1,23 @@
+package CursoJava.anteSpringFase01;
+
+public class Exemplo01 {
+    //exceções
+   /*
+   1️⃣5️⃣ DESAFIO FINAL (nível júnior 🔥)
+
+Crie um sistema de produtos:
+
+Classe Produto:
+
+nome
+preço
+estoque
+
+Funcionalidades:
+
+adicionar produto
+listar produtos
+vender produto (diminuir estoque)
+se estoque < 0 → lançar exceção
+    */
+}
