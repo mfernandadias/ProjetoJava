42615b0e800ebcce9267dbbcb8f0983c5a1edeb0
Fernanda Dias <mariadosantodias@gmail.com>
2026-05-11 07:36:54 -0300

    Create Exercicio180a.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo9Strings/Exercicio180a.java
@@ -0,0 +1,15 @@
+package CursoJava.aExercicios200.capitulo9Strings;
+
+public class Exercicio180a {
+    /*
+    implemente um programa qe receba duas strings do usuário
+    e exiba a concatenação delas
+     */
+    public static void main(String[] args){
+        String name = "Maria";
+        String sobrenome = "Dias";
+
+        System.out.println(name + " " + sobrenome);
+
+    }
+}
