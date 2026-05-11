4a90074465079ccf261fa73746050e864b14ecb6
Fernanda Dias <mariadosantodias@gmail.com>
2026-05-11 07:34:49 -0300

    Create Produto.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo7Exercicio128/Produto.java
@@ -0,0 +1,12 @@
+package CursoJava.aExercicios200.capitulo7Exercicio128;
+
+public class Produto {
+    /*
+    exercicio 132: classe produto
+    Crie uma classe chamada Produto com os atributos nome, preco e quantidade.
+Adicione um método para calcular o valor total do estoque (preco * quantidade) e outro
+método para exibir os detalhes do produto. No método main(), crie dois objetos e teste os
+métodos.
+     */
+
+}
