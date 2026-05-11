6ec9f3fb9f21cea2f4f8fca804b871e91a4ff63b
Fernanda Dias <mariadosantodias@gmail.com>
2026-05-11 07:36:46 -0300

    Create Exercicio180.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo9Strings/Exercicio180.java
@@ -0,0 +1,14 @@
+package CursoJava.aExercicios200.capitulo9Strings;
+
+public class Exercicio180 {
+    /*
+    crie um programa que receba uma string e exiba o número de caracteres que contém
+     */
+    public static void main(String[] args){
+        String nome = "fernanda";
+
+        System.out.println(nome.length());
+
+
+    }
+}
