aa1d4b2ed950ec1b0d703a5c3e49a1cf00606756
Fernanda Dias <mariadosantodias@gmail.com>
2026-05-11 07:38:42 -0300

    Create Exercicio198.java

---
+++ src/main/java/CursoJava/aExercicios200/capitulo9Strings/Exercicio198.java
@@ -0,0 +1,17 @@
+package CursoJava.aExercicios200.capitulo9Strings;
+
+public class Exercicio198 {
+    /*
+     escreva um programa que compare uma string
+     */
+    public static void main(String[] args){
+        String name = "maria";
+        String nm = "maria";
+
+        int resultado = name.compareTo(nm);
+
+
+
+
+    }
+}
