3ec73516eb2b675d6e53032170dfb56ffaecec6f
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-08 15:08:21 -0300

    Create Exemplo02.java

---
+++ src/main/java/CursoJava/nelio/secao10/Exemplo02.java
@@ -0,0 +1,15 @@
+package CursoJava.nelio.secao10;
+
+import java.util.Scanner;
+
+public class Exemplo02 {
+    public static void main(String[] args){
+        /*
+        fazer um programa para ler um número inteiro N e a altura de N pessoas. Armazenas as N alturas em um vetor.
+        Em seguida, mostrar a altura média dessas pessoas
+         */
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
