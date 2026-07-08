56e777e2e411cd5ac0085af40bff64a33ba6532b
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-08 15:09:54 -0300

    Create TaxService.java

---
+++ src/main/java/cursoSpringBoot/parte0/TaxService.java
@@ -0,0 +1,35 @@
+package cursoSpringBoot.parte0;
+
+import java.time.LocalDate;
+
+public class TaxService {
+    public static void main(String[] args){
+
+        //Classe LocalDate
+        /*
+        Representa apenas uma data, sem horário
+         */
+        LocalDate  data = LocalDate.now();
+
+        System.out.println(data);
+
+        /*
+        Aprenda:
+        LocalDate.now();
+        LocalDate.of();
+        getYear()
+        getDayofMonth()
+        plusDays()
+        minusDays()
+         */
+
+
+
+
+
+
+
+
+
+    }
+}
