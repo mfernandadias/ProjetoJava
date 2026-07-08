781fa3d067bf302348f8303ccb7d19df3759b596
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-08 15:09:29 -0300

    Create Exercicio09.java

---
+++ src/main/java/CursoJava/nelio/secao10/Exercicio09.java
@@ -0,0 +1,10 @@
+package CursoJava.nelio.secao10;
+
+public class Exercicio09 {
+    public static void main (String[] args){
+        /*
+        crie um vetor com 10 posições e preencha com números pares
+         */
+        //int[] num =
+    }
+}
