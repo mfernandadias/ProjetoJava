290f3dc53f73c3d5a8211b43e81e363221682d1a
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-08 15:10:31 -0300

    Create Program01.java

---
+++ src/main/java/CursoJava/nelio/secao10/Program01.java
@@ -0,0 +1,14 @@
+package CursoJava.nelio.secao10;
+
+public class Program01 {
+}
+
+/*
+Valores padrão
+Quando alocamos(new) qualquer tipo estruturado(classe ou array), são atribuídos valores padrão aos seus elementos
+* números: 0
+* boolean: false
+* char: caractere código 0
+* objeto: null
+
+ */
