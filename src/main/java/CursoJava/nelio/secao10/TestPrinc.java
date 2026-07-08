b6bd1418c812b959107f48c3cabf4398e0fbaa50
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-08 15:10:33 -0300

    Create TestPrinc.java

---
+++ src/main/java/CursoJava/nelio/secao10/TestPrinc.java
@@ -0,0 +1,23 @@
+package CursoJava.nelio.secao10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestPrinc {
+    public static void main(String[] args){
+        /*
+
+         */
+        List<String> list = new ArrayList<>();
+
+        list.add("Maria");
+        list.add("Alex");
+        list.add("Ana");
+        list.add("MArco");
+
+
+        System.out.println(list.size());
+
+
+    }
+}
