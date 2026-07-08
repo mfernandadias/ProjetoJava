d118ea2429788152717eb075f7e36fc3382fb8b8
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-08 15:09:48 -0300

    Create ExercicioMatriz.java

---
+++ src/main/java/CursoJava/nelio/secao10/ExercicioMatriz.java
@@ -0,0 +1,14 @@
+package CursoJava.nelio.secao10;
+
+public class ExercicioMatriz {
+    public static void main(String[] args){
+        /*
+        Fazer um programa para ler dois números inteiros M e N, e depois ler uma matriz de M
+        linhas por N colunas conteno números inteiros, podendo haver repetições. Em seguida,
+        ler  um número inteiro X que pertende à matriz. Para cada ocorrência de X, mostrar
+        os valores à esquerda, acima, à direita e abaixo de X, quando houver, conforme exemplo
+        */
+
+
+    }
+}
