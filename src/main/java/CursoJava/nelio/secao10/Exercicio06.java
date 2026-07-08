cdc0284a541e8c62ed8cd2cc2bc7605ea4f9851b
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-08 15:08:39 -0300

    Create Exercicio06.java

---
+++ src/main/java/CursoJava/nelio/secao10/Exercicio06.java
@@ -0,0 +1,10 @@
+package CursoJava.nelio.secao10;
+
+public class Exercicio06 {
+    public static void main(String[] args){
+        /*
+        fazer um programa para ler um número inteiro N e os dados(nome e preço) de N produtos.
+        
+         */
+    }
+}
