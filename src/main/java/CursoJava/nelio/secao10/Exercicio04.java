dbb2f631cb4a5de45257682c9673ef37a69b3f50
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-08 15:08:34 -0300

    Create Exercicio04.java

---
+++ src/main/java/CursoJava/nelio/secao10/Exercicio04.java
@@ -0,0 +1,15 @@
+package CursoJava.nelio.secao10;
+
+public class Exercicio04 {
+    public static void main(String[] args){
+        /*
+        Leia um vetor e mostre o maior valor.
+         */
+        int[] num = {9, 12, 16, 8, 10, 6, 8, 2, 4, 8, 10};
+        int maior = num[0];
+
+        for(int i = 1; i < num[i]; i++){
+            
+        }
+    }
+}
