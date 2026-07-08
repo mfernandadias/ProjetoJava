cb235e78fab5d61f44f72e571f0913c545a331d0
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-08 15:08:41 -0300

    Create Exercicio07.java

---
+++ src/main/java/CursoJava/nelio/secao10/Exercicio07.java
@@ -0,0 +1,18 @@
+package CursoJava.nelio.secao10;
+
+public class Exercicio07 {
+    public static void main(String[] args){
+        /*
+
+         */
+        int[] num = {9, 10, 15, 6, 8, 2, 1, 9};
+
+        for(Integer i : num){
+            if(i % 2 == 0){
+                System.out.print(i + " ");
+            }
+        }
+
+
+    }
+}
