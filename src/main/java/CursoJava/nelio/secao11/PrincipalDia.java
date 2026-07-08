62f2977ef87afdf4036e802d0cac3d02e2cd752b
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-08 15:10:15 -0300

    Create PrincipalDia.java

---
+++ src/main/java/CursoJava/nelio/secao11/PrincipalDia.java
@@ -0,0 +1,12 @@
+package CursoJava.nelio.secao11;
+
+public class PrincipalDia {
+    public static void main(String[] args){
+        int num = 4;
+
+        //num >= 1 && num <= 12
+        if(num >= 1 && num <= 07 ){
+
+        }
+    }
+}
