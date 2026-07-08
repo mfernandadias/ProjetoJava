1acd45e6c8ca70e0946ee036ff33e9de34873dce
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-08 15:07:50 -0300

    Create Exercicio01.java

---
+++ src/main/java/CursoJava/java10x/iniciante/Exercicio01.java
@@ -0,0 +1,10 @@
+package CursoJava.java10x.iniciante;
+
+public class Exercicio01 {
+    public static void descricao(){
+        System.out.println("Bem-vindo ao Java");
+    }
+    public static void main(String[] args){
+        descricao();
+    }
+}
