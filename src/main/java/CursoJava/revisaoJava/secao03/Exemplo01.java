c1cee76fcf9028e66edc1af4fa0150d828ae179a
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-08 15:09:58 -0300

    Create Exemplo01.java

---
+++ src/main/java/CursoJava/revisaoJava/secao03/Exemplo01.java
@@ -0,0 +1,12 @@
+package CursoJava.revisaoJava.secao03;
+
+import java.util.Arrays;
+
+public class Exemplo01 {
+    public static void main(String[] args){
+
+        int[] valores = {10, 12, 3, 1, 9, 10};
+        System.out.println(Arrays.toString(valores));
+
+    }
+}
