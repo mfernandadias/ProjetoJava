aa7cf1885369ac2192a9a85366637c32a44e1cf0
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-28 17:28:52 -0300

    Create Aula17EntradaESaida.java

---
+++ src/main/java/CursoJava/revisaoJava/secao03/Aula17EntradaESaida.java
@@ -0,0 +1,18 @@
+package CursoJava.revisaoJava.secao03;
+
+public class Aula17EntradaESaida {
+    public static void main(String[] args){
+        /*
+        System.out.print
+        System.out.println
+        System.out.printf
+        - %d
+        - %f
+        - %s
+        - %n
+        Locale
+        Como concatenar vários elementos em um mesmo comando de escrita
+        Exemplos
+         */
+    }
+}
