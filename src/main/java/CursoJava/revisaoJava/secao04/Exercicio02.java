b708f77bd42f74399b8b3a0092e1b9d3ee728ace
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-28 17:29:03 -0300

    Create Exercicio02.java

---
+++ src/main/java/CursoJava/revisaoJava/secao04/Exercicio02.java
@@ -0,0 +1,8 @@
+package CursoJava.revisaoJava.secao04;
+
+public class Exercicio02 {
+    public static void main(String[] args){
+        //leia um numero inteiro e informe se ele é positivo, negativo ou zero
+        
+    }
+}
