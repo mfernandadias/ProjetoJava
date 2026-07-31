2bddb6dea764a7df4bd309c349e80ed4762a68e6
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-31 11:09:12 -0300

    Create Exemplo04.java

---
+++ src/main/java/CursoJava/nelio/secao10Arrays/Exemplo04.java
@@ -0,0 +1,13 @@
+package CursoJava.nelio.secao10Arrays;
+
+public class Exemplo04 {
+    public static void main(String[] args){
+        //for each facilita percorrer arrays e coleções
+        int[] numeros = {10, 20, 30, 40};
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+    }
+}
