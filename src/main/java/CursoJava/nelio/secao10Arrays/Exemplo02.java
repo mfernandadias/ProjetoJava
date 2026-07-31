6e1b76751f308e16b5b0d1fc8a3bd793744e7f76
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-31 11:09:08 -0300

    Create Exemplo02.java

---
+++ src/main/java/CursoJava/nelio/secao10Arrays/Exemplo02.java
@@ -0,0 +1,11 @@
+package CursoJava.nelio.secao10Arrays;
+
+public class Exemplo02 {
+    /*
+    Por que null?
+    Porque posições de um vetor de String começam como:
+    null
+    ou seja:
+    Não existe uma String armazenada naquela posição
+     */
+}
