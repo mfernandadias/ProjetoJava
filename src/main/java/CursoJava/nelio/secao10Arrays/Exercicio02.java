392085183485c77bb0dd5cee0b42352259130679
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-31 11:15:41 -0300

    Create Exercicio02.java

---
+++ src/main/java/CursoJava/nelio/secao10Arrays/Exercicio02.java
@@ -0,0 +1,12 @@
+package CursoJava.nelio.secao10Arrays;
+
+public class Exercicio02 {
+    public static void main(String[] args){
+        double[] notas = {9, 8, 7.5, 9, 10};
+
+        for(int i = 0; i < notas.length; i++){
+
+        }
+
+    }
+}
