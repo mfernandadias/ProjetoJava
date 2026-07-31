94baeba9be306329750c86511f2f4fe842903ee4
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-07-31 11:09:10 -0300

    Create Exemplo03.java

---
+++ src/main/java/CursoJava/nelio/secao10Arrays/Exemplo03.java
@@ -0,0 +1,16 @@
+package CursoJava.nelio.secao10Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exemplo03 {
+    public static void main(String[] args){
+        List<Integer> numeros = new ArrayList<>();
+
+        //List<int> numeros; //Errado
+
+        List<Integer> num;
+
+
+    }
+}
