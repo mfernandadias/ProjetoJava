e5be6181524861b0ce443d3678ab2cebfb2fbd55
Maria Fernanda Dias <91575360+mfernandadias@users.noreply.github.com>
2026-09-20 16:58:09 -0300

    Create Pessoa.java

---
+++ src/main/java/CursoJava/aprendizado/aprendeizado3/Pessoa.java
@@ -0,0 +1,12 @@
+package CursoJava.aprendizado.aprendeizado3;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private double altura;
+
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+}
